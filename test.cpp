27e1f24750d5e41f89383a7ae5390ad37f408109
Andrew Witten <awitten007@gmail.com>
2025-07-14 23:13:45 -0400

    little test file

---
+++ test.cpp
@@ -0,0 +1,63 @@
+
+
+#include <sys/mman.h>
+#include <iostream>
+#include <fstream>
+#include <unistd.h>
+
+#define PAGE_SIZE 4096
+
+unsigned long get_minor_page_faults() {
+  std::ifstream f("/proc/self/stat");
+  int idx = 0;
+  for (;;) {
+    ++idx;
+    std::string entry;
+    f >> entry;
+    if (idx != 10) {
+      continue;
+    }
+    return std::stoul(entry);
+  }
+}
+
+
+void write_into_pages(char* buf, int num_pages) {
+  for (int i = 0; i < num_pages; ++i) {
+    *(buf + PAGE_SIZE*i) = 'a';
+  }
+}
+
+int do_stuff(int);
+
+int main(int argc, char** argv) {
+  int num_pages = std::stoi(argv[1]);
+  if (argc >= 3) {
+    for (;;) {
+      if (int ret = do_stuff(num_pages)) {
+        return ret;
+      }
+      sleep(1);
+    }
+  }
+  return do_stuff(num_pages);
+}
+
+int do_stuff(int num_pages) {
+  static char* buf = (char*)mmap(NULL, PAGE_SIZE*num_pages, PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_SHARED, -1, 0);
+
+  if (buf == MAP_FAILED) {
+    fprintf(stderr, "mmap failed");	
+    return EXIT_FAILURE;
+  }
+  get_minor_page_faults();
+  unsigned long minor_faults1 = get_minor_page_faults();
+  write_into_pages(buf, num_pages);
+  unsigned long minor_faults2 = get_minor_page_faults();
+  std::cout << minor_faults1 << ',' << minor_faults2 << std::endl;
+  write_into_pages(buf, num_pages);
+  unsigned long minor_faults3 = get_minor_page_faults();
+  std::cout << minor_faults2 << ',' << minor_faults3 << std::endl;
+  return 0;
+  
+}
