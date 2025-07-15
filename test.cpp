a4e63ecf8af81a1c47dcc0bb9467378a6fdc5e46
Andrew Witten <awitten007@gmail.com>
2025-07-15 11:30:12 -0400

    great

--- test.cpp
+++ test.cpp
@@ -1,5 +1,6 @@
 
 
+#include <cstdint>
 #include <sys/mman.h>
 #include <iostream>
 #include <fstream>
@@ -43,21 +44,41 @@ int main(int argc, char** argv) {
   return do_stuff(num_pages);
 }
 
+inline uint64_t __attribute__((always_inline)) rdtsc() {
+	uint32_t low, high;
+
+	asm volatile(
+		"mfence \n"
+		"lfence \n"
+		"rdtsc \n"
+		"lfence \n"
+		: "=a" (low), "=d" (high)
+	);
+
+	uint64_t tsc1 = low | ((uint64_t)high << 32);
+	return tsc1;
+}
+
+
 int do_stuff(int num_pages) {
-  static char* buf = (char*)mmap(NULL, PAGE_SIZE*num_pages, PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_SHARED, -1, 0);
+  static char* buf = (char*)mmap(NULL, PAGE_SIZE*num_pages, PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_SHARED, 1, 0);
 
   if (buf == MAP_FAILED) {
-    fprintf(stderr, "mmap failed");	
+    fprintf(stderr, "mmap failed");
     return EXIT_FAILURE;
   }
-  get_minor_page_faults();
+  get_minor_page_faults(); rdtsc();
   unsigned long minor_faults1 = get_minor_page_faults();
+  auto c1 = rdtsc();
   write_into_pages(buf, num_pages);
+  auto c2 = rdtsc();
   unsigned long minor_faults2 = get_minor_page_faults();
-  std::cout << minor_faults1 << ',' << minor_faults2 << std::endl;
+  std::cout << minor_faults2 - minor_faults1 << ',' << c2 - c1 << std::endl;
+  auto c3 = rdtsc();
   write_into_pages(buf, num_pages);
+  auto c4 = rdtsc();
   unsigned long minor_faults3 = get_minor_page_faults();
-  std::cout << minor_faults2 << ',' << minor_faults3 << std::endl;
+  std::cout << minor_faults3 - minor_faults2 << ',' << c4 - c3 << std::endl;
   return 0;
-  
+
 }
